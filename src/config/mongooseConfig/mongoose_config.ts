a1e00b7e6e8714fdfbec5ec007c354a3a16bbb79
Tonmoy <tonmoybiswas19122002@gmail.com>
2024-03-23 20:02:12 +0530

    db reconnect on disconnect added

--- src/config/mongooseConfig/mongoose_config.ts
+++ src/config/mongooseConfig/mongoose_config.ts
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 export async function connectDb() {
-    try{
+    try {
         await mongoose.connect(process.env.MONGO_URL || "");
         console.log("socket server connected to database");
+        let db = mongoose.connection;
+        db.on('disconnected',async function () {
+            // console.log('Disconnected from MongoDB');
+            await mongoose.connect(process.env.MONGO_URL || "");
+        });
     }
-    catch(err:any){
+    catch (err: any) {
         console.log(err.message);
     }
 }
